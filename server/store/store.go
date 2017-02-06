c2c4f721158d0158660363412852fbc8e36612ee
or-else <or.else@gmail.com>
2017-02-05 21:50:11 -0800

    addind deletion by list

--- server/store/store.go
+++ server/store/store.go
@@ -1,32 +1,3 @@
-/******************************************************************************
- *
- *  Copyright (C) 2014-2016 Tinode, All Rights Reserved
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or (at your
- *  option) any later version.
- *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- *  or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, see <http://www.gnu.org/licenses>.
- *
- *  This code is available under licenses for commercial use.
- *
- *  File        :  store.go
- *
- ******************************************************************************
- *
- *  Description :
- *
- *  Database abastraction layer
- *
- *****************************************************************************/
-
 package store
 
 import (
