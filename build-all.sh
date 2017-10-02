421439929861fce56c2be7d1de38b156f3ee4a0e
or-else <or.else@gmail.com>
2017-10-02 11:09:38 -0700

    fixing build tags

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-~/go/bin/gox -osarch="linux/amd64" -ldflags "-X main.buildstamp=`date -u '+%Y%m%dT%H:%M:%SZ'` -tags rethinkdb" ./server
-~/go/bin/gox -osarch="linux/amd64" -ldflags "-tags rethinkdb" ./tinode-db
+~/go/bin/gox -osarch="linux/amd64" -ldflags "-X main.buildstamp=`date -u '+%Y%m%dT%H:%M:%SZ'`" -tags rethinkdb ./server
+~/go/bin/gox -osarch="linux/amd64" -tags rethinkdb ./tinode-db
 ~/go/bin/gox -osarch="linux/amd64" ./keygen
