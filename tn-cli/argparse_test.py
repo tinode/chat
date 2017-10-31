ec28cd9cf32b57719549f3b2953a8cb5fcb935f0
or-else <or.else@gmail.com>
2017-10-30 21:31:00 -0700

    expanded message definitions

--- tn-cli/argparse_test.py
+++ tn-cli/argparse_test.py
@@ -1,56 +1,76 @@
 import argparse
-
-class JoinArgs(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, ' '.join(values))
+import shlex
 
 def parse_command(cmd):
     """Parses command line input into a ClientMsg"""
-    parts = cmd.split(" ")
-    parser = argparse.ArgumentParser(prog=parts[0])
+    parts = shlex.split(cmd)
+    parser = None
     if parts[0] == "acc":
-        parser.add_argument('--user', default=None, help='user ID to change')
+        parser = argparse.ArgumentParser(prog=parts[0], description='Create or alter an account')
+        parser.add_argument('--user', default=None, help='ID of the account to update')
+        parser.add_argument('--scheme', default="basic", help='authentication scheme, default=basic')
         parser.add_argument('--secret', default=None, help='secret for authentication')
-        parser.add_argument('--scheme', default="basic", help='authentication scheme')
         parser.add_argument('--uname', default=None, help='user name for basic authentication')
         parser.add_argument('--password', default=None, help='password for basic authentication')
         parser.add_argument('--do-login', action='store_true', help='login with the newly created account')
-        parser.add_argument('--tags', action=None, help='tags for user discovery, e.g. "--tags=email:abc@example.com,tel:1234567890"')
-        parser.add_argument('--desc', default=None, help='user parameters')
+        parser.add_argument('--tags', action=None, help='tags for user discovery, comma separated list without spaces')
+        parser.add_argument('--fn', default=None, help='user\'s human name')
+        parser.add_argument('--photo', default=None, help='avatar file name')
+        parser.add_argument('--private', default=None, help='user\'s private info')
+        parser.add_argument('--auth', default=None, help='default access mode for authenticated users')
+        parser.add_argument('--anon', default=None, help='default access mode for anonymous users')
     elif parts[0] == "login":
+        parser = argparse.ArgumentParser(prog=parts[0], description='Authenticate current session')
         parser.add_argument('--scheme', default="basic")
         parser.add_argument('secret', nargs='?', default=argparse.SUPPRESS)
         parser.add_argument('--secret', dest='secret', default=None)
         parser.add_argument('--uname', default=None)
         parser.add_argument('--password', default=None)
     elif parts[0] == "sub":
+        parser = argparse.ArgumentParser(prog=parts[0], description='Subscribe to topic')
         parser.add_argument('topic', nargs='?', default=argparse.SUPPRESS, help='topic to subscribe to')
         parser.add_argument('--topic', dest='topic', default=None, help='topic to subscribe to')
     elif parts[0] == "leave":
+        parser = argparse.ArgumentParser(prog=parts[0], description='Detach or unsubscribe from topic')
         parser.add_argument('topic', nargs='?', default=argparse.SUPPRESS, help='topic to detach from')
         parser.add_argument('--topic', dest='topic', default=None, help='topic to detach from')
         parser.add_argument('--unsub', action='store_true', help='detach and unsubscribe from topic')
     elif parts[0] == "pub":
-        parser.add_argument('topic', nargs='?', default=argparse.SUPPRESS)
-        parser.add_argument('--topic', dest='topic', default=None)
-        parser.add_argument('content', nargs='*', default=argparse.SUPPRESS, action=JoinArgs, help='message to send')
-        parser.add_argument('--content', nargs='*', dest='content', action=JoinArgs, help='message to send')
+        parser = argparse.ArgumentParser(prog=parts[0], description='Send message to topic')
+        parser.add_argument('topic', nargs='?', default=argparse.SUPPRESS, help='topic to publish to')
+        parser.add_argument('--topic', dest='topic', default=None, help='topic to publish to')
+        parser.add_argument('content', nargs='?', default=argparse.SUPPRESS, help='message to send')
+        parser.add_argument('--content', dest='content', help='message to send')
     elif parts[0] == "get":
+        parser = argparse.ArgumentParser(prog=parts[0], description='Query topic for messages or metadata')
         print("Not implemented: " + parts[0])
         return None
     elif parts[0] == "set":
+        parser = argparse.ArgumentParser(prog=parts[0], description='Update topic metadata')
         print("Not implemented: " + parts[0])
         return None
     elif parts[0] == "del":
+        parser = argparse.ArgumentParser(prog=parts[0], description='Delete message(s), subscription or topic')
         print("Not implemented: " + parts[0])
         return None
     elif parts[0] == "note":
-        print("Not implemented: " + parts[0])
-        return None
+        parser = argparse.ArgumentParser(prog=parts[0], description='Send notification to topic')
+        parser.add_argument('topic', nargs='?', default=argparse.SUPPRESS, help='topic to notify')
+        parser.add_argument('--topic', dest='topic', default=None, help='topic to notify')
+        parser.add_argument('--what', help='notification type')
+        parser.add_argument('--seq', help='value being reported')
     else:
         print "Unrecognized:", parts[0]
         print "Possible commands:"
-        print "\tacc, login, sub, leave, pub, get, set, del, note"
+        print "\tacc\t- create account"
+        print "\tlogin\t- authenticate"
+        print "\tsub\t- subscribe to topic"
+        print "\tleave\t- detach or unsubscribe from topic"
+        print "\tpub\t- post message to topic"
+        print "\tget\t- query topic for metadata or messages"
+        print "\tset\t- update topic metadata"
+        print "\tdel\t- delete message(s), topic or subscription"
+        print "\tnote\t- send notification"
         print "\n\tType <command> -h for help"
         return None
 
