c0a469faec11c0b4deae6b9f66ecf017234edf64
or-else <or.else@gmail.com>
2017-11-25 22:10:24 -0800

    roll back version - its not fixed yet

--- tn-cli/tn-cli.py
+++ tn-cli/tn-cli.py
@@ -14,7 +14,7 @@ import model_pb2 as pb
 import model_pb2_grpc as pbx
 
 APP_NAME = "tn-cli"
-VERSION = "0.14"
+VERSION = "0.13"
 
 # Dictionary wich contains lambdas to be executed when server response is received
 onCompletion = {}
