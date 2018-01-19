2ce9fb1c7d0085f21d5c11bc8043a88f2ece2d1c
or-else <or.else@gmail.com>
2018-01-19 14:46:26 -0800

    adding two new methods to UidGenerator to simplify sql support

--- server/store/types/uidgen.go
+++ server/store/types/uidgen.go
@@ -29,7 +29,9 @@ func (ug *UidGenerator) Init(workerID uint, key []byte) error {
 	return err
 }
 
-// Get generates a unique weakly encryped id it so ids are random-looking.
+// Get generates a unique weakly-encryped random-looking ID.
+// The Uid is a unit64 with the highest bit possibly set which makes it
+// incompatible with go's pre-1.9 sql package.
 func (ug *UidGenerator) Get() Uid {
 	buf, err := getIDBuffer(ug)
 	if err != nil {
@@ -38,7 +40,9 @@ func (ug *UidGenerator) Get() Uid {
 	return Uid(binary.LittleEndian.Uint64(buf))
 }
 
-// GetStr generates a unique id then returns it as base64-encrypted string.
+// GetStr generates the same unique ID as Get then returns it as
+// base64-encoded string. Slightly more efficient than calling Get()
+// then base64-encoding the result.
 func (ug *UidGenerator) GetStr() string {
 	buf, err := getIDBuffer(ug)
 	if err != nil {
@@ -61,4 +65,28 @@ func getIDBuffer(ug *UidGenerator) ([]byte, error) {
 	ug.cipher.Encrypt(dst, src)
 
 	return dst, nil
+}
+
+// DecodeUid takes an encrypted Uid and decrypts it into a non-negative int64.
+// This is needed for go/sql compatibility where uint64 with high bit
+// set is unsupported and possibly for other uses such as MySQL's recommendation
+// for non-random primary keys.
+func (ug *UidGenerator) DecodeUid(uid Uid) int64 {
+	var src = make([]byte, 8)
+	var dst = make([]byte, 8)
+	binary.LittleEndian.PutUint64(src, uint64(uid))
+	ug.cipher.Decrypt(dst, src)
+	return int64(binary.LittleEndian.Uint64(dst))
+}
+
+// EncodeUint64 takes a positive int64 and encrypts it into a Uid.
+// This is needed for go/sql compatibility where uint64 with high bit
+// set is unsupported  and possibly for other uses such as MySQL's recommendation
+// for non-random primary keys.
+func (ug *UidGenerator) EncodeInt64(val int64) Uid {
+	var src = make([]byte, 8)
+	var dst = make([]byte, 8)
+	binary.LittleEndian.PutUint64(src, uint64(val))
+	ug.cipher.Encrypt(dst, src)
+	return Uid(binary.LittleEndian.Uint64(dst))
 }
