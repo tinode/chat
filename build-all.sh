674b17f13ecf9f645efc543edb239f2616aeeb91
or-else <or.else@gmail.com>
2018-03-03 21:03:31 -0800

    fix broken rethinkdb adapter

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Cross-compiling script using https://github.com/mitchellh/gox
+# I use this to compile the Linux version of the server on my Mac.
+
 ~/go/bin/gox -osarch="linux/amd64" -ldflags "-X main.buildstamp=rdb.`date -u '+%Y%m%dT%H:%M:%SZ'`" -tags rethinkdb ./server
 ~/go/bin/gox -osarch="linux/amd64" -tags rethinkdb ./tinode-db
 ~/go/bin/gox -osarch="linux/amd64" ./keygen
