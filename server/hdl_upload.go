7a3bd7dfaab92e02888cd935dba97ef5169be135
or-else <or.else@gmail.com>
2018-05-26 07:51:10 -0700

    create large file handler

---
+++ server/hdl_upload.go
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *
+ *  Description :
+ *
+ *    Handler of large file uploads. Default handler saves files to the
+ *    file system at the configured endpoint.
+ *
+ *****************************************************************************/
+
+package main
+
+// largeFileUpload receives files from HTTP(S) and saves them to local file
+// system.
+func largeFileUpload(wrt http.ResponseWriter, req *http.Request) {
+}
