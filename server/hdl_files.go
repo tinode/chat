3c29a8babf270b63fc0c915b872caaabe4056ed7
or-else <or.else@gmail.com>
2018-06-28 10:35:55 +0300

    bug fix: incorrect handling of file upload errors

--- server/hdl_files.go
+++ server/hdl_files.go
@@ -136,15 +136,14 @@ func largeFileUpload(wrt http.ResponseWriter, req *http.Request) {
 		if strings.Contains(err.Error(), "request body too large") {
 			log.Println("Uploaded file is too large", err)
 			writeHttpResponse(ErrTooLarge("", "", now))
-			if strings.Contains(err.Error(), "unexpected EOF") {
-				log.Println("Upload interrupted by client", err)
-				writeHttpResponse(ErrMalformed("", "", now))
-			} else {
-				log.Println("Upload error", err)
-				writeHttpResponse(ErrMalformed("", "", now))
-			}
-			return
+		} else if strings.Contains(err.Error(), "unexpected EOF") {
+			log.Println("Upload interrupted by client", err)
+			writeHttpResponse(ErrMalformed("", "", now))
+		} else {
+			log.Println("Upload error", err)
+			writeHttpResponse(ErrMalformed("", "", now))
 		}
+		return
 	}
 	fdef := types.FileDef{}
 	fdef.Id = store.GetUidString()
