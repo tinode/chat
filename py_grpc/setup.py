a058cb6778675e77009c85c72931284b7b86b8e3
or-else <or.else@gmail.com>
2018-08-31 21:10:41 +0300

    python 3.5 verified

--- py_grpc/setup.py
+++ py_grpc/setup.py
@@ -29,6 +29,7 @@ setuptools.setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
