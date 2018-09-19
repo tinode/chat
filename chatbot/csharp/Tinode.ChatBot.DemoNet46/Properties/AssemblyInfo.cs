5aff554cab805ce16d4269928b0eb4e04e7f395e
BenDerPan <BenDerPan@163.com>
2018-09-19 16:17:52 +0800

    update nuget information

--- chatbot/csharp/Tinode.ChatBot.DemoNet46/Properties/AssemblyInfo.cs
+++ chatbot/csharp/Tinode.ChatBot.DemoNet46/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Tinode.ChatBot.DemoNet46")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tinode")]
 [assembly: AssemblyProduct("Tinode.ChatBot.DemoNet46")]
 [assembly: AssemblyCopyright("https://github.com/tinode/chat")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.15.5.0")]
+[assembly: AssemblyFileVersion("0.15.5.0")]
