4162473d6bd49bf4173c4ca7d1a87633e3535085
or-else <or.else@gmail.com>
2018-12-08 21:27:29 +0300

    rest auth server is functional

--- rest-auth/auth.py
+++ rest-auth/auth.py
@@ -1,6 +1,14 @@
 from flask import Flask, jsonify, make_response
+import base64
+import json
+
+dummy_data = {}
 
 app = Flask(__name__)
+
+def parse_secret(ecoded_secret):
+    secret = base64.b64decode(ecoded_secret)
+    return secret.split(':')
 
 @app.route('/')
 def index():
@@ -15,8 +23,36 @@ def add():
 def auth():
     if not request.json:
         return jsonify({'err': 'malformed'})
-
-    return ""
+    uname, password = parse_secret(request.json.secret)
+    if dummy_data[uname]:
+        if dummy_data[uname]['password'] != password:
+            # Wrong password
+            return jsonify({'err': 'failed'})
+        if dummy_data[uname]['uid']:
+            # We have uname -> uid mapping
+            jsonify({
+                'rec': {
+                    'uid': dummy_data[uname]['uid'],
+                    'authlvl': dummy_data[uname]['authlvl'],
+                }
+            })
+        else:
+            # This is the first login. Tell Tinode to create a new account.
+            jsonify({
+                'rec': {
+                    'authlvl': dummy_data[uname]['authlvl'],
+                    'tags': dummy_data[uname]['tags']
+                },
+                'newacc': {
+                    'auth': dummy_data[uname]['auth'],
+                    'anon': dummy_data[uname]['anon'],
+                    'public': dummy_data[uname]['public'],
+                    'private': dummy_data[uname]['private']
+                }
+            })
+        return jsonify({'err': 'unsupported'})
+    else:
+        return jsonify({'err': 'not found'})
 
 @app.route('/checkunique', methods=['POST'])
 def checkunique():
@@ -40,7 +76,19 @@ def link():
     if not rec or not rec['uid'] or not secret:
         return jsonify({'err': 'malformed'})
 
-    # TODO: save the link account <-> secret to database.
+    # Save the link account <-> secret to database.
+    uname, password = parse_secret(secret)
+    if not dummy_data[uname]:
+        # Unknown user name
+        return jsonify({'err': 'not found'})
+    if dummy_data[uname]['uid']:
+        # Already linked
+        return jsonify({'err': 'duplicate value'})
+
+    # Save updated data to file
+    dummy_data[uname]['uid'] = rec['uid']
+    with open('dummy_data.json', 'w') as outfile:
+        json.dump(dummy_data, outfile)
 
     # Success
     return jsonify({})
@@ -53,5 +101,13 @@ def upd():
 def not_found(error):
     return make_response(jsonify({'err': 'not found'}), 404)
 
+@app.errorhandler(405)
+def not_found(error):
+    return make_response(jsonify({'err': 'method not allowed'}), 405)
+
 if __name__ == '__main__':
+    # Load previously saved dummy data. Dummy data contains
+    # tinode user id <-> user name mapping and data for account creation.
+    with open('dummy_data.json') as infile:
+        dummy_data = json.load(infile)
     app.run(debug=True)
