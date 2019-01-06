8d758261a291ccd7f5153f856419190f7d0a5c5d
or-else <or.else@gmail.com>
2019-01-06 14:47:24 -0800

    add missing stats.go

---
+++ server/stats.go
@@ -0,0 +1,97 @@
+// Logic related to expvar handling: reporting live stats such as
+// session and topic counts, memory usage etc.
+// The stats updates happen in a separate go routine to avoid
+// locking on main logic routines.
+
+package main
+
+import (
+	"expvar"
+	"log"
+	"net/http"
+)
+
+type varUpdate struct {
+	// Name of the variable to update
+	varname string
+	// Integer value to publish
+	count int64
+	// Treat the count as an increment as opposite to the final value.
+	inc bool
+}
+
+// Initialize stats reporting through expvar.
+func statsInit(mux *http.ServeMux, path string) {
+	if path == "" || path == "-" {
+		return
+	}
+
+	mux.Handle(path, expvar.Handler())
+	globals.statsUpdate = make(chan *varUpdate, 1024)
+
+	go statsUpdater()
+
+	log.Printf("stats: variables exposed at '%s'", path)
+}
+
+// Register integer variable. Don't check for initialization.
+func statsRegisterInt(name string) {
+	expvar.Publish(name, new(expvar.Int))
+}
+
+// Async publish int variable.
+func statsSet(name string, val int64) {
+	if globals.statsUpdate != nil {
+		select {
+		case globals.statsUpdate <- &varUpdate{name, val, false}:
+		default:
+		}
+	}
+}
+
+// Async publish an increment (decrement) to int variable.
+func statsInc(name string, val int) {
+	if globals.statsUpdate != nil {
+		select {
+		case globals.statsUpdate <- &varUpdate{name, int64(val), true}:
+		default:
+		}
+	}
+}
+
+// Stop publishing stats.
+func statsShutdown() {
+	if globals.statsUpdate != nil {
+		globals.statsUpdate <- nil
+	}
+}
+
+// The go routine which actually publishes stats updates.
+func statsUpdater() {
+loop:
+	for {
+		select {
+		case upd := <-globals.statsUpdate:
+			if upd == nil {
+				// Nil update means shutdown
+				close(globals.statsUpdate)
+				break loop
+			}
+
+			// Handle var update
+			if ev := expvar.Get(upd.varname); ev != nil {
+				// Intentional panic if the ev is not *expvar.Int.
+				intvar := ev.(*expvar.Int)
+				if upd.inc {
+					intvar.Add(upd.count)
+				} else {
+					intvar.Set(upd.count)
+				}
+			} else {
+				panic("stats: update to unknown variable " + upd.varname)
+			}
+		}
+	}
+
+	log.Println("stats: shutdown")
+}
