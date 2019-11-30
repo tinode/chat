a0379d61ddb826d1a4a95d0ff220924a39aefdcb
or-else <or.else@gmail.com>
2019-11-30 10:53:45 +0300

    explain reason for changing value

--- server/session.go
+++ server/session.go
@@ -786,6 +786,7 @@ func (s *Session) onLogin(msgID string, timestamp time.Time, rec *auth.Rec, miss
 			// Authenticate the session.
 			s.uid = rec.Uid
 			s.authLvl = rec.AuthLevel
+			// Reset expiration time.
 			rec.Lifetime = 0
 		}
 		features |= auth.FeatureValidated
