f3027f1a6d9931a58c3c86ade4c42f8625a27aba
or-else <or.else@gmail.com>
2020-03-14 16:27:51 +0300

    add build stamp to main.go

--- monitoring/exporter/main.go
+++ monitoring/exporter/main.go
@@ -26,6 +26,16 @@ func (l promHTTPLogger) Println(v ...interface{}) {
 	log.Println(v...)
 }
 
+// Build version number defined by the compiler:
+// 		-ldflags "-X main.buildstamp=value_to_assign_to_buildstamp"
+// Reported to clients in response to {hi} message.
+// For instance, to define the buildstamp as a timestamp of when the server was built add a
+// flag to compiler command line:
+// 		-ldflags "-X main.buildstamp=`date -u '+%Y%m%dT%H:%M:%SZ'`"
+// or to set it to git tag:
+// 		-ldflags "-X main.buildstamp=`git describe --tags`"
+var buildstamp = "undef"
+
 func main() {
 	log.Printf("Tinode metrics exporter.")
 
