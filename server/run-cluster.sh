00df5718dfc07d5206da93b3b0fd86eb678f62ef
or-else <or.else@gmail.com>
2020-03-30 12:29:03 +0300

    run-cluster.sh updated to use custom config

--- server/run-cluster.sh
+++ server/run-cluster.sh
@@ -7,44 +7,60 @@ ALL_NODE_NAMES=( one two three )
 # Port where the first node will listen for client connections over http
 HTTP_BASE_PORT=6080
 # Port where the first node will listen for gRPC intra-cluster connections.
-GRPC_BASE_PORT=6090
+GRPC_BASE_PORT=16060
 
-# Allow for non-default config file to be specifid on the command line like config=file_name
-if [ ! -z "$config" ] ; then
-  TINODE_CONF=$config
-else
-  TINODE_CONF="./tinode.conf"
-fi
+# Assign command line parameters to variables.
+# for line in $@; do
+#  eval "$line"
+#done
 
-case "$1" in
-  start)
-    echo 'Running cluster on localhost, ports 6080-6082'
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  shift
+  echo "$key"
+  case "$key" in
+    -c|--config)
+      config=$1
+      shift # value
+      ;;
+    start)
+      if [ ! -z "$config" ] ; then
+        TINODE_CONF=$config
+      else
+        TINODE_CONF="tinode.conf"
+      fi
 
-    HTTP_PORT=$HTTP_BASE_PORT
-    GRPC_PORT=$GRPC_BASE_PORT
-    for NODE_NAME in "${ALL_NODE_NAMES[@]}"
-    do
-      # Start the node
-      ./server -config=${TINODE_CONF} -cluster_self=$NODE_NAME -listen=:${HTTP_PORT} -grpc_listen=:${GRPC_PORT} &
-      # Save PID of the node to a temp file.
-      # /var/tmp/ does not requre root access.
-      echo $!> "/var/tmp/tinode-${NODE_NAME}.pid"
-      # Increment ports for the next node.
-      HTTP_PORT=$((HTTP_PORT+1))
-      GRPC_PORT=$((GRPC_PORT+1))
-    done
-    ;;
-  stop)
-    echo 'Stopping cluster'
+      echo "HTTP ports 6080-6082, gRPC ports 16060-16062, config ${config}"
 
-    for NODE_NAME in "${ALL_NODE_NAMES[@]}"
-    do
-      # Reda PIDs of running nodes from temp files and kill them.
-      kill `cat /var/tmp/tinode-${NODE_NAME}.pid`
-      # Clean up: delete temp files.
-      rm "/var/tmp/tinode-${NODE_NAME}.pid"
-    done
-    ;;
-  *)
-    echo $"Usage: $0 {start|stop} [ config=<path_to_tinode.conf> ]"
-esac
+      HTTP_PORT=$HTTP_BASE_PORT
+      GRPC_PORT=$GRPC_BASE_PORT
+      for NODE_NAME in "${ALL_NODE_NAMES[@]}"
+      do
+        # Start the node
+        ./server -config=${TINODE_CONF} -cluster_self=${NODE_NAME} -listen=:${HTTP_PORT} -grpc_listen=:${GRPC_PORT} &
+        # Save PID of the node to a temp file.
+        # /var/tmp/ does not requre root access.
+        echo $!> "/var/tmp/tinode-${NODE_NAME}.pid"
+        # Increment ports for the next node.
+        HTTP_PORT=$((HTTP_PORT+1))
+        GRPC_PORT=$((GRPC_PORT+1))
+      done
+      exit 0
+      ;;
+    stop)
+      echo 'Stopping cluster'
+
+      for NODE_NAME in "${ALL_NODE_NAMES[@]}"
+      do
+        # Read PIDs of running nodes from temp files and kill them.
+        kill `cat /var/tmp/tinode-${NODE_NAME}.pid`
+        # Clean up: delete temp files.
+        rm "/var/tmp/tinode-${NODE_NAME}.pid"
+      done
+      exit 0
+      ;;
+    *)
+      echo $"Usage: $0 {start|stop} [ --config <path_to_tinode.conf> ]"
+      exit 1
+  esac
+done
