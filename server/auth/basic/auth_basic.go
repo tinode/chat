34be70fb25ec785952e79ab5fff85fd1cd86bc2e
Trieu Nguyen <aihturie@gmail.com>
2020-07-16 11:14:48 +0700

    Check for duplicate uname when updating basic auth
    Check for duplicate uname when user is changing unique name.

--- server/auth/basic/auth_basic.go
+++ server/auth/basic/auth_basic.go
@@ -167,6 +167,11 @@ func (a *authenticator) UpdateRecord(rec *auth.Rec, secret []byte) (*auth.Rec, e
 		uname = login
 	} else if err = a.checkLoginPolicy(uname); err != nil {
 		return nil, err
+	} else if uid, _, _, _, err := store.Users.GetAuthUniqueRecord(a.name, uname); err != nil {
+		return nil, err
+	} else if !uid.IsZero() {
+		// .User is changing unique name, check for duplicate unique name
+		return nil, types.ErrDuplicate
 	}
 
 	if err = a.checkPasswordPolicy(password); err != nil {
