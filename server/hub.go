13441865b84e6cb865f20bf5b3400ff1a0280383
aforge <aforge@>
2020-10-15 22:52:33 -0700

    Make sure sess.inflightReqs is not nil before accessing it.

--- server/hub.go
+++ server/hub.go
@@ -211,7 +211,9 @@ func (h *Hub) run() {
 				select {
 				case t.reg <- join:
 				default:
-					join.sess.inflightReqs.Done()
+					if join.sess.inflightReqs != nil {
+						join.sess.inflightReqs.Done()
+					}
 					join.sess.queueOut(ErrServiceUnavailableReply(join.pkt, join.pkt.Timestamp))
 					log.Println("hub.join loop: topic's reg queue full", join.pkt.RcptTo, join.sess.sid, " - total queue len:", len(t.reg))
 				}
