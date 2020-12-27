f0494fe538b2c93fabd04341a1dd6e08f75f38de
or-else <or.else@gmail.com>
2020-12-27 11:14:18 -0800

    more linter fixes

--- server/concurrency/goroutinepool.go
+++ server/concurrency/goroutinepool.go
@@ -9,6 +9,7 @@ package concurrency
 // Task represents a work task to be run on the specified thread pool.
 type Task func()
 
+// GoRoutinePool is a pull of Go routines with associated locking mechanism.
 type GoRoutinePool struct {
 	// Work queue.
 	work chan Task
