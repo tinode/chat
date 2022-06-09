14a8f5925ba786c901ad046da5f1c6993eb1c1c3
or-else <or.else@gmail.com>
2022-06-09 10:47:42 -0700

    typo null -> nil, add call timout to params

--- server/session.go
+++ server/session.go
@@ -755,6 +755,7 @@ func (s *Session) hello(msg *ClientComMessage) {
 			"maxTagCount":        globals.maxTagCount,
 			"maxFileUploadSize":  globals.maxFileUploadSize,
 			"iceServers":         globals.iceServers,
+			"callTineout":        globals.callEstablishmentTimeout,
 		}
 
 		// Set ua & platform in the beginning of the session.
