57056971c661ec2a8a2754503ba95f29d575b13d
or-else <or.else@gmail.com>
2022-06-20 19:45:39 -0700

    copy og:image to distro

--- build-all.sh
+++ build-all.sh
@@ -62,6 +62,7 @@ then
 
   cp ./server/static/img/*.png ./releases/tmp/static/img
   cp ./server/static/img/*.svg ./releases/tmp/static/img
+  cp ./server/static/img/*.jpeg ./releases/tmp/static/img
   cp ./server/static/audio/*.m4a ./releases/tmp/static/audio
   cp ./server/static/css/*.css ./releases/tmp/static/css
   cp ./server/static/index.html ./releases/tmp/static
