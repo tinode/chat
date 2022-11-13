2fc18d5607d1ff3bd85f8f74548a651a86562d3a
aforge <aforge@>
2022-11-12 17:26:58 -0800

    Do not increment unread counter upon 'read' push notifications.

--- server/user.go
+++ server/user.go
@@ -1060,7 +1060,13 @@ func userUpdater() {
 					allUids = append(allUids, uid)
 				}
 
-				allUnread := unreadUpdater(allUids, 1, true)
+				var delta int
+				if upd.PushRcpt.Payload.What != "read" {
+					delta = 1
+				} else {
+					delta = 0
+				}
+				allUnread := unreadUpdater(allUids, delta, true)
 				for uid, unread := range allUnread {
 					rcptTo := upd.PushRcpt.To[uid]
 					// Handle update
