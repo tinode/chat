97613c134543bab4ea1253486816047b86d20916
or-else <or.else@gmail.com>
2025-06-22 16:11:04 +0300

    update collation in mysql schema file

--- server/db/mysql/schema.sql
+++ server/db/mysql/schema.sql
@@ -10,7 +10,7 @@
 
 DROP DATABASE IF EXISTS tinode;
 
-CREATE DATABASE tinode CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE tinode CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 USE tinode;
 
